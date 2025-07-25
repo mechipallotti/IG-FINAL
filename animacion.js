73d05cc10e7f00badb138010026ab35c24c9ed7d
Mechi Pallotti <mpallotti7@gmail.com>
2025-07-25 19:34:27 -0300

    avances en pag autor, personajes y libro

--- animacion.js
+++ animacion.js
@@ -7,3 +7,21 @@
   imagen.addEventListener("mouseleave", () => {
     imagen.src = "img/estrella.gif"; // imagen original
   });
+
+
+// Animacion de imagenes al hacer click 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const imagenes = document.querySelectorAll(".animacion-img");
+
+  imagenes.forEach((img) => {
+    img.addEventListener("mouseenter", () => {
+      img.classList.add("hovered");
+    });
+
+    img.addEventListener("mouseleave", () => {
+      img.classList.remove("hovered");
+    });
+  });
+});
+
