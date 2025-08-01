a2d44dcbbedcdb65c68994e8f76040bad2fac489
Mechi Pallotti <mpallotti7@gmail.com>
2025-07-31 22:24:07 -0300

    un monton de cambios; se crearon dos supaginas. se termino subpagina de linea de tiempo acomode general de carpetas, titulos y navegadores

--- animacion.js
+++ animacion.js
@@ -1,3 +1,5 @@
+// CAMBIO DE IMAGEN EN HOMEPAGE
+
 const imagen = document.getElementById("imagenEstrella");
 if (imagen) {
   imagen.addEventListener("mouseenter", () => {
@@ -7,7 +9,7 @@ if (imagen) {
     imagen.src = "img/estrella.gif";
   });
 }
-// Animacion de imagenes al hacer click 
+// CAMBIO DE FRASES EN FRASES.HTML
 
 document.addEventListener("DOMContentLoaded", function () {
   const imagenes = document.querySelectorAll(".animacion-img");
@@ -80,12 +82,14 @@ document.addEventListener("DOMContentLoaded", () => {
 // INTERACCION IMAGEN EN ABOUT ME.
 
 const aboutImage = document.getElementById('change-img');
+if (aboutImage) {
         const originalSrc = aboutImage.src;
-        const newSrc = 'img/yo2.gif'; // Cambia esto por la URL de tu nueva imagen
+        const newSrc = '../about-me/aboutme-img/yo2.gif'; 
 
         aboutImage.addEventListener('click', function() {
             aboutImage.src = (aboutImage.src === originalSrc) ? newSrc : originalSrc;
         });
+      }
 
 // ANIMACION EN SUBPAGINA BILLY
 
@@ -95,3 +99,87 @@ const aboutImage = document.getElementById('change-img');
       location.href = destino;
     }
 
+
+// GALERIA DE FOTOS
+document.addEventListener("DOMContentLoaded", () => {
+
+  const imagenesGaleria = [
+     {
+      src: "../fragmento/fragmento-img/fragmento1.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento2.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento3.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento4.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento5.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento6.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento7.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento8.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento9.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento10.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento11.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento12.jpg",
+    },
+    {
+      src: "../fragmento/fragmento-img/fragmento13.jpg",
+
+    },
+    
+  ];
+let indice = 0;
+
+  const imgEl = document.getElementById("galeria-img");
+
+  const mostrarImagen = () => {
+    imgEl.src = imagenesGaleria[indice].src;
+  };
+
+  mostrarImagen();
+
+  document.getElementById("anterior").addEventListener("click", () => {
+    indice = (indice - 1 + imagenesGaleria.length) % imagenesGaleria.length;
+    mostrarImagen();
+  });
+
+  document.getElementById("siguiente").addEventListener("click", () => {
+    indice = (indice + 1) % imagenesGaleria.length;
+    mostrarImagen();
+  });
+});
+
+// ANIMACION EN  SUBPAGINA: LINEA DE TIEMPO DE KURT VONNEGUT
+
+document.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelectorAll(".timeline-item");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
+
+  items.forEach(item => observer.observe(item));
+});
